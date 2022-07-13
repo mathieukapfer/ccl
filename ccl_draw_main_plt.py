0b31f3443951a6e6cb106ca6ae8460cb31018cd4
Mathieu Kapfer <mkapfer@kalrayinc.com>
2022-07-13 15:27:12 +0200

    [smem map] add stat

--- ccl_draw_main_plt.py
+++ ccl_draw_main_plt.py
@@ -8,6 +8,7 @@ from ccl_parser.ccl_parse2 import ccl_file_parser
 
 import pickle as plk
 
+
 def draw_rectangle(ax, color, x1, y1, x2, y2):
     """
     Plot corner and adding rectangle as patch above
@@ -262,12 +263,107 @@ def test_draw_smem_layout():
     plk.dump(fig, open('smem_fig.pickle', 'wb'))
 
 
+# test_draw_smem_layout()
+
+
 def load_pickle():
     # import other modules needed to work with the figure, such as np, plt etc.
     figx = plk.load(open('smem_fig.pickle', 'rb'))
-    figx.show()
+    figx.show(block=False)
 
 
-test_draw_smem_layout()
 
 # load_pickle()
+
+
+def draw_stat(nodes):
+
+    fig = plt.figure()
+
+    # stats by cluster
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+    ax = [ax1, ax2]
+
+    # list of events by cluster
+    events = [[], []]
+
+    # 1 - Create events list
+    for index in nodes:
+        node = nodes[index]
+
+        # create event
+        cluster = get_cluster(node.get('defining resource'))
+
+        if int(node.get('define', -1)) > 0:
+            events[cluster].append({
+                'date': int(node.get('define')),
+                'event': 'PE',
+                'value': 1})
+
+            events[cluster].append({
+                'date': int(node.get('end_define')),
+                'event': 'PE',
+                'value': -1})
+
+    # 2 - Create nb PEs curve
+    for events_by_cluster in events:
+
+        cluster = events.index(events_by_cluster)
+        sorted_events = sorted(events_by_cluster, key=lambda item: item['date'])
+
+        print(sorted_events)
+
+        #
+        nb_active_pes = 0
+        date = 0
+        previous_nb_active_pes = 0
+        previous_date = 0
+        x = list()
+        y = list()
+
+        for event in sorted_events:
+            date = event['date']
+
+            print("cluster:{}, time:{}, #PE:{}".format(
+                cluster, event['date'], nb_active_pes))
+
+            if (previous_date != date):
+                # added new point previously computed
+                print("plot {},{}, {},{}".format(
+                    previous_date, nb_active_pes,
+                    previous_date, previous_nb_active_pes,
+                ))
+                x.append(previous_date)
+                y.append(previous_nb_active_pes)
+                x.append(previous_date)
+                y.append(nb_active_pes)
+                previous_date = date
+                previous_nb_active_pes = nb_active_pes
+
+            # sum each event of the same date
+            nb_active_pes += event['value']
+            print("delta {},{}".format(date, nb_active_pes))
+
+        # add last point
+        x.append(date)
+        y.append(previous_nb_active_pes)
+        x.append(date)
+        y.append(nb_active_pes)
+
+        # plot cluster curve
+        ax[cluster].plot(
+            x, y,
+            linewidth=2, marker='o', c='black', markersize=2)
+
+        plt.show(block=False)
+
+
+def test_stat():
+    """
+    """
+    nodes = ccl_file_parser(filename)
+    return draw_stat(nodes)
+
+
+test_stat()
